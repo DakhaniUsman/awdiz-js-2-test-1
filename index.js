50c9fad5d2fcf7f96db928cd40e56ed307f607a3
Dakhani Usman <115381340+DakhaniUsman@users.noreply.github.com>
2025-02-02 13:35:10 +0530

    JS 2 Test 1

--- index.js
+++ index.js
@@ -78,9 +78,17 @@ function QuestionFour(array) {
 var array = [1, 2, 3, 4, 5, 6];
 
 function QuestionFive(array) {
+  let sum = 0;
 
-  console.log("array :", array)
+  console.log("array :", array);
 
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 == 0) {
+      sum = sum + array[i];
+    }
+  }
+
+  return sum;
 }
 
 console.log(QuestionFive(array));
